bb647ad2ae753e70dfd6c980040fd3c492a6f4b8
野村優羽 <@MSI>
2023-04-22 22:54:08 +0900

    スライド26まで

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -3,16 +3,12 @@ float4 main() : SV_TARGET
 	return float4(1.0f, 1.0f, 1.0f, 1.0f);
 }
 
-struct PixelShaderOutput {
-
+struct PixelshaderOutput {
 	float32_t4 color : SV_TARGET0;
-
 };
 
 PixelShaderOutput main() {
-
-	pixelShaderOutput output;
+	PixelshaderOutput output;
 	output.color = float32_t4(1.0, 1.0, 1.0, 1.0);
 	return output;
-
 }
--- main.cpp
+++ main.cpp
@@ -88,9 +88,47 @@ IDxcBlob* CompileShader(
 	shaderSourceBuffer.Encoding = DXC_CP_UTF8; //UTF8の文字コードであることを通知
 
 	//Compileする
-	
+	LPCWSTR arguments[] = {
+		filePath.c_str(), //コンパイル対象のhlslファイル名
+		L"-E",L"main",//エントリーポイントの指定,基本的にmain以外にはしない
+		L"-T", profile, //ShaderProfileの設定
+		L"-zi", L"-Qembed_debug", //デバッグ用の情報を埋め込む
+		L"-Od", //最適化を外しておく
+		L"-Zpr", //メモリレイアウトは作成
+	};
+	//実際にShaderをコンパイルする
+	IDxcResult* shaderResult = nullptr;
+	hr = dxCompiler->Compile(
+		&shaderSourceBuffer,//読み込んだファイル
+		arguments,//コンパイルオプション
+		_countof(arguments),//コンパイルオプションの数
+		includeHandler,//includeがふくまれた諸々
+		IID_PPV_ARGS(&shaderResult)//コンパイル結果
+	);
+	//コンパイルエラーではなくdixが起動できないなど致命的な状況
+	assert(SUCCEEDED(hr));
+
 	//警告・エラーがでていないか確認する
+	//警告・エラーが出てたらログに出して止める
+	IDxcBlobUtf8* shaderError = nullptr;
+	shaderResult->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&shaderError), nullptr);
+	if (shaderError != nullptr && shaderError->GetStringLength() != 0) {
+		Log(shaderError->GetStringPointer());
+		assert(false);
+	}
+	
 	//compile結果を受け取って返す
+	//コンパイル結果から実行用のバイナリ部分を取得
+	IDxcBlob* shaderBlob = nullptr;
+	hr = shaderResult->GetOutput(DXC_OUT_OBJECT, IID_PPV_ARGS(&shaderBlob), nullptr);
+	assert(SUCCEEDED(hr));
+	//成功したログを出す
+	Log(ConvertString(std::format(L"Compile Succeeded, path:{}, profile:{}\n", filePath, profile)));
+	//もう使わないリソースを解放
+	shaderSource->Release();
+	shaderResult->Release();
+	//実行用のバイナリを返却
+	return shaderBlob;
 
 }
 
